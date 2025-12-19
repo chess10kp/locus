08f69daaa1cbd7af30003fed669183c34daf4c7b
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2025-12-19 13:11:15 -0500

    fix: disable memory corruption checking cause it keeps breaking

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 # GTK4 Layer Shell requires LD_PRELOAD for correct linking order with libwayland
 # This may cause memory corruption issues in some versions of GTK4 Layer Shell
 # See: https://github.com/wmww/gtk4-layer-shell/blob/main/linking.md
-LD_PRELOAD=/usr/lib/libgtk4-layer-shell.so PWD=/home/sigma/projects/repos/locus uv run python ~/projects/repos/locus/main.py & disown
+#
+# Workaround for malloc corruption with Python 3.13:
+MALLOC_CHECK_=0 MALLOC_PERTURB_=0 LD_PRELOAD=/usr/lib/libgtk4-layer-shell.so PWD=/home/sigma/projects/repos/locus uv run python ~/projects/repos/locus/main.py & disown
