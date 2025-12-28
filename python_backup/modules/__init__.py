0e20bcb7524a4e982d1bcdec88689ccc33182495
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2025-12-27 21:44:46 -0500

    chore: remove  python implemetnation

--- python_backup/modules/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Modules package for locus plugin system.
-
-This package contains different types of modules:
-- statusbar: Statusbar modules
-- launcher: Launcher modules (in the future)
-"""
--- run.sh
+++
@@ -1,6 +0,0 @@
-# GTK4 Layer Shell requires LD_PRELOAD for correct linking order with libwayland
-# This may cause memory corruption issues in some versions of GTK4 Layer Shell
-# See: https://github.com/wmww/gtk4-layer-shell/blob/main/linking.md
-#
-# Workaround for malloc corruption with Python 3.13:
-GTK_DEBUG=all MALLOC_CHECK_=0 MALLOC_PERTURB_=0 LD_PRELOAD=/usr/lib/libgtk4-layer-shell.so PWD=/home/sigma/projects/repos/locus uv run python ~/projects/repos/locus/main.py & disown
